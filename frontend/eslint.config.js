37bd2628d39afae111989c127224ee1f8430de9a
prisa <jglaveaga@gmaill.com>
2025-11-16 15:40:58 -0600

    chore: fix frontend lint toolchain

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,7 +2,6 @@ import js from '@eslint/js';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
-import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default tseslint.config(
   js.configs.recommended,
@@ -18,5 +17,4 @@ export default tseslint.config(
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
   },
-  eslintConfigPrettier,
 );
